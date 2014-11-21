82eda361434355826aacc84213f55c684462e8bb
Fedya Skitsko <fedya@skitsko.com>
2014-11-21 21:02:57 +0200

    Initial Commit

---
+++ iBeaconDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconDemo
+//
+//  Created by Fedya Skitsko on 11/21/14.
+//  Copyright (c) 2014 LabWerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
