bcd0ca87623fc89c7bcfffb8214986eff6e8f197
Fedya Skitsko <fedya@skitsko.com>
2014-11-22 12:34:25 +0200

    Change minor label with strongest beacon

--- iBeaconDemo/ViewController.swift
+++ iBeaconDemo/ViewController.swift
@@ -12,19 +12,29 @@ import CoreLocation
 class ViewController: UIViewController {
     
     @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var minorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         numberLabel.text = "Unknown"
         
+        //@TODO please check also if Bluetooth is enabled
+        
         BeaconManager.sharedInstance.authorizationStateHandler = { state in
             if state == .Authorized {
                 self.view.backgroundColor = UIColor.greenColor()
                 
-                let beaconRegion = BeaconRegion(proximityUUID: NSUUID(UUIDString: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"), major: 1, identifier: "new_beacon_region")
+                let beaconRegion = BeaconRegion(proximityUUID: NSUUID(UUIDString: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"), major: 5555, identifier: "new_beacon_region")
                 
                 beaconRegion.rangeHandler = { beacons in
+                    var sortedBeacons = beacons.sorted { $0.rssi > $1.rssi }.reverse() as [CLBeacon!]
+
+                    for beacon in sortedBeacons {
+                        if beacon.proximity != .Unknown {
+                            self.minorLabel.text = "\(beacon.minor)\n \(beacon.rssi)db"
+                        }
+                    }
                 }
                 
                 beaconRegion.stateHandler = { state in
